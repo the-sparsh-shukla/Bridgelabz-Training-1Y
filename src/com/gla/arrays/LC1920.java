2631038a9422bb5bd19830a73b0d93370af704d8
SPARSH SHUKLA <sparsh.shukla006@gmail.com>
2026-02-05 21:47:24 +0530

    Class_Problem

---
+++ src/com/gla/arrays/LC1920.java
@@ -0,0 +1,16 @@
+package com.gla.arrays;
+
+public class LC1920{
+    public int[] buildArray(int[] nums) {
+
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            ans[i] = nums[nums[i]];
+        }
+
+        return ans;
+    }
+}
+
---
+++ src/com/gla/arrays/PrintMatrix.java
@@ -0,0 +1,4 @@
+package com.gla.arrays;
+
+public class PrintMatrix {
+}
